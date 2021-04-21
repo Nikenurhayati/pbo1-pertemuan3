dc443c41704fba640bd4304e6aee2f6b211e03bb
Nikenurhayati <nikenurhayati37@gmail.com>
2021-04-21 18:52:57 +0700

    toUppercase untuk membuat huruf kapital toLowercase untuk membuat huruf kecil substring(6) untuk menampilkan huruf dari 6 sampai seterusnya substring (0,6) untuk menampilkan huruf dari 0 sampai sebelum 6

--- src/OperasiString.java
+++ src/OperasiString.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class OperasiString {
     public static void main(String[] args) {
         String kota = "Banjarmasin" ;
@@ -6,5 +8,10 @@ public class OperasiString {
         char[] uniskachar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskachar);
         System.out.println(uniskaString);
+
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(6));
+        System.out.println(kota.substring(0,6));
     }
 }
