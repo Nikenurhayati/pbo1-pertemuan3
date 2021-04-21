fac5f7959f9112006879a511427357d70742c03b
Nikenurhayati <nikenurhayati37@gmail.com>
2021-04-21 19:03:37 +0700

    operator Binary Assigment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai sebelum variable
+    // baris 4-6 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Nike Nur Hayati";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.99;
+
+}
